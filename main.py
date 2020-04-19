c1b6cd4a05afd58ae5782dc1d97c17768f56da89
lwd-temp <lwd-temp@github.com>
2020-04-19 17:40:45 +0800

    i.........

---
+++ main.py
@@ -0,0 +1,21 @@
+from browser import document
+# HappyBirthdayZWTForBrython
+# Author:LWD
+# This code is used as an Easter Egg.
+# lwd-temp@Github.com 2020 版权所有 © 保留所有权利。
+def main():
+    # For the most adorable one.
+    import datetime
+    date=datetime.datetime.today()
+    if date.month==4:
+        if date.day>=20:
+            document <= "Happy birthday ZWT!"+"<a href="https://www.lwdedu.ml/happy-birthday-zwt-win32/dist/main.exe">Get the Binary.</a>"
+    if date.month==5:
+        if date.day==5:
+            count=0
+            while count!=4:
+                document <= "Happy birthday ZWT!"+"<a href="https://www.lwdedu.ml/happy-birthday-zwt-win32/dist/main.exe">Get the Binary.</a>"
+                count=count+1
+        if date.day<=15:
+            document <= "Happy birthday ZWT!"+"<a href="https://www.lwdedu.ml/happy-birthday-zwt-win32/dist/main.exe">Get the Binary.</a>"
+main()
